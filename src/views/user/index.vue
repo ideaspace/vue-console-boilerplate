115145015cdb7aa2a931953834c2639ecc96ab44
ideaspace <foot_print@163.com>
2019-03-03 18:36:31 +0800

    fix: 更新页面布局

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,44 +1,42 @@
 <template>
   <view-container title="修改密码">
-    <el-card>
-      <el-row>
-        <el-col :span="8">
-          <el-form :model="changePwdForm" ref="changePwdForm" :rules="rules" label-width="120px">
-            <el-form-item prop="password" label="输入原密码">
-              <el-input
-                v-model.trim="changePwdForm.password"
-                autocomplete="off"
-                type="password"
-                placeholder="输入原密码"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="newPassword" label="输入新密码">
-              <el-input
-                v-model.trim="changePwdForm.newPassword"
-                placeholder="支持大小写英文字母、数字，长度6-20个字符"
-                :maxlength="20"
-                :minlength="6"
-                type="password"
-                autocomplete="off"
-              ></el-input>
-            </el-form-item>
-            <el-form-item prop="checkPassword" label="确认新密码">
-              <el-input
-                v-model.trim="changePwdForm.checkPassword"
-                type="password"
-                :maxlength="18"
-                :minlength="6"
-                placeholder="请确认密码"
-                autocomplete="off"
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="onConfirm">确认</el-button>
-            </el-form-item>
-          </el-form>
-        </el-col>
-      </el-row>
-    </el-card>
+    <el-row>
+      <el-col :span="8">
+        <el-form :model="changePwdForm" ref="changePwdForm" :rules="rules" label-width="120px">
+          <el-form-item prop="password" label="输入原密码">
+            <el-input
+                    v-model.trim="changePwdForm.password"
+                    autocomplete="off"
+                    type="password"
+                    placeholder="输入原密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="newPassword" label="输入新密码">
+            <el-input
+                    v-model.trim="changePwdForm.newPassword"
+                    placeholder="支持大小写英文字母、数字，长度6-20个字符"
+                    :maxlength="20"
+                    :minlength="6"
+                    type="password"
+                    autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="checkPassword" label="确认新密码">
+            <el-input
+                    v-model.trim="changePwdForm.checkPassword"
+                    type="password"
+                    :maxlength="18"
+                    :minlength="6"
+                    placeholder="请确认密码"
+                    autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onConfirm">确认</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </view-container>
 </template>
 
