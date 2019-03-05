e07b6fc042360d0dd57ea99f2720c883d96aa32c
ideaspace <foot_print@163.com>
2019-03-05 20:18:43 +0800

    feat: 新增部分组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import {VueConstructor} from 'vue';
 import ViewContainer from './layout/Container.vue';
-import SpinLoader from './SpinLoader.vue';
-import SvgIcon from './SvgIcon.vue';
+import SpinLoader from './spin/SpinLoader.vue';
+import SvgIcon from './icon/SvgIcon.vue';
+import CountTo from './count/CountTo.vue';
+import TableTree from './table/TableTree.vue';
 
 interface CompsList {
   [key: string]: VueConstructor;
@@ -11,6 +13,8 @@ export const Comps: CompsList = {
   ViewContainer,
   SpinLoader,
   SvgIcon,
+  CountTo,
+  TableTree,
 };
 
 const requireSvgIcon = require.context('@/assets/icons', false, /\.svg$/);
